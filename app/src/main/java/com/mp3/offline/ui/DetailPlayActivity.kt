2144cac42f205007deb837a0dab58b8b27e4cd72
alankurniadi12 <alankurniadi53@gmail.com>
2021-01-19 14:37:46 +0700

    update detailActivity

--- app/src/main/java/com/mp3/offline/ui/DetailPlayActivity.kt
+++ app/src/main/java/com/mp3/offline/ui/DetailPlayActivity.kt
@@ -1,32 +1,31 @@
 package com.mp3.offline.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
-import android.view.OrientationEventListener
+import android.os.Message
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.LinearInterpolator
 import android.view.animation.RotateAnimation
-import android.widget.HorizontalScrollView
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.rotationMatrix
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
+import com.mp3.offline.R
 import com.mp3.offline.databinding.ActivityDetailPlayBinding
 import com.mp3.offline.model.Model
-import java.util.concurrent.TimeUnit
-import kotlin.system.exitProcess
 
 class DetailPlayActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailPlayBinding
     private var dataDetail: Model? = null
     private var mp: MediaPlayer? = null
+    private var totalTime: Int = 0
     var rotateAnimation: RotateAnimation? = null
     lateinit var mAdView : AdView
 
@@ -35,110 +34,120 @@ class DetailPlayActivity : AppCompatActivity() {
         binding = ActivityDetailPlayBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        //Lock screen rotation
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        //Get data mp3
+        dataDetail = intent.getParcelableExtra("keyData") as Model
+
+        //Set Ui
+        binding.tvSongTitle.text = dataDetail!!.title
+        binding.tvArtist.text = dataDetail!!.artist
+        binding.imgCover.setImageResource(dataDetail!!.photo)
+
+        //Media Player
+        mp = MediaPlayer.create(this, dataDetail!!.mp3)
+        mp!!.isLooping = true
+        totalTime = mp!!.duration
+
+        //Set progressbar
+        binding.positionBar.max = totalTime
+        binding.positionBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    if (fromUser) mp!!.seekTo(progress)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+            }
+        )
+
+        Thread(Runnable {
+            while (mp != null) {
+                var msg = Message()
+                try {
+                    msg.what = mp!!.currentPosition
+                    handler.sendMessage(msg)
+                    Thread.sleep(1000)
+                }catch (e: InterruptedException){
+                }
+            }
+        }).start()
+
         //AdMob Code
         MobileAds.initialize(this) {}
         mAdView = binding.adView
         val adRequest = AdRequest.Builder().build()
         mAdView.loadAd(adRequest)
+    }
 
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    @SuppressLint("HandlerLeak")
+    var handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+            var currentPosition = msg.what
 
-        dataDetail = intent.getParcelableExtra("keyData") as Model
+            binding.positionBar.progress = currentPosition
 
-        binding.tvSongTitle.text = dataDetail!!.title
-        binding.tvArtist.text = dataDetail!!.artist
-        binding.imgCover.setImageResource(dataDetail!!.photo)
+            var elapsedTime = createTimeLabel(currentPosition)
+            binding.tvElapsedTime.text = elapsedTime
 
-        setPlayer(dataDetail!!.mp3)
-
-        binding.btnPlaybackground.setOnClickListener {
-            val intent = Intent(Intent.ACTION_MAIN)
-            intent.addCategory(Intent.CATEGORY_HOME)
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            startActivity(intent)
+            var remainingTime = createTimeLabel(totalTime - currentPosition)
+            binding.tvRemainingTime.text = "-$remainingTime"
         }
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        mp?.release()
+    private fun createTimeLabel(time: Int): String {
+        var timeLabel = ""
+        var min = time / 1000 / 60
+        var sec = time / 1000 % 60
+
+        timeLabel = "$min:"
+        if (sec < 10) timeLabel += "0"
+        timeLabel += sec
+
+        return timeLabel
     }
 
-    private fun setPlayer(mp3: Int) {
-        binding.imgPlay.setOnClickListener {
-            if (mp == null){
-                mp = MediaPlayer.create(this, mp3)
-                initialiseSeekBar()
-            }
-            mp?.start()
+    fun playButtonClick(v: View) {
+        if (mp!!.isPlaying) {
+            mp!!.pause()
+            binding.imgPlay.setImageResource(R.drawable.ic__play_circle_24)
 
-            binding.imgPause.visibility = View.VISIBLE
-            binding.imgPlay.visibility = View.GONE
+            rotateAnimation!!.cancel()
+        } else {
+            mp!!.start()
+            binding.imgPlay.setImageResource(R.drawable.ic_pause_circle_24)
 
             rotateAnimation = RotateAnimation(0f, 360f,
-            Animation.RELATIVE_TO_SELF, 0.5f,
-            Animation.RELATIVE_TO_SELF, 0.5f)
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f)
             rotateAnimation!!.duration = 15000
             rotateAnimation!!.interpolator = LinearInterpolator()
             rotateAnimation!!.repeatCount =Animation.INFINITE
 
             binding.imgCover.startAnimation(rotateAnimation)
         }
-
-        binding.imgPause.setOnClickListener {
-            if (mp != null) mp?.pause()
-
-            rotateAnimation!!.cancel()
-            binding.imgPause.visibility = View.GONE
-            binding.imgPlay.visibility = View.VISIBLE
-        }
-
-        binding.imgStop.setOnClickListener {
-            if (mp != null) {
-                mp?.stop()
-                mp?.reset()
-                mp?.release()
-                mp = null
-            }
-
-            rotateAnimation!!.cancel()
-            binding.imgPause.visibility = View.GONE
-            binding.imgPlay.visibility = View.VISIBLE
-        }
-
-        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if (fromUser) mp?.seekTo(progress)
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-            }
-        })
     }
 
-    private fun initialiseSeekBar() {
-        binding.seekBar.max = mp!!.duration
-
-        val handler = Handler()
-        handler.postDelayed(object: Runnable {
-            override fun run() {
-                try {
-                    binding.seekBar.progress = mp!!.currentPosition
-                    handler.postDelayed(this, 1000)
-                } catch (e: Exception){
-                    binding.seekBar.progress = 0
-                    e.printStackTrace()
-                }
-            }
-        }, 0)
-        val duration = mp!!.duration
-        val time = String.format("%02d:%02d",
-            TimeUnit.MILLISECONDS.toMinutes(duration.toLong()),
-            TimeUnit.MILLISECONDS.toSeconds(duration.toLong()) -
-                    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration.toLong())))
-        binding.tvDuration.text = time
+    fun playBtnBackground(v: View) {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_HOME)
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
     }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        mp?.stop()
+    }
+
 }
