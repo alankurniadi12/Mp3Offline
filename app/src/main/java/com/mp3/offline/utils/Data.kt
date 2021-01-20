522ba8d6f538e00a04ada6d6dd5a57c897847d56
alankurniadi12 <alankurniadi53@gmail.com>
2021-01-20 18:12:34 +0700

    update ui

--- app/src/main/java/com/mp3/offline/utils/Data.kt
+++ app/src/main/java/com/mp3/offline/utils/Data.kt
@@ -9,43 +9,43 @@ object Data {
         val dataMp3 = ArrayList<Model>()
 
         dataMp3.add(
-            Model(1, "Ya Habibal Qolbi", "Nissa", R.drawable.nissa1, R.raw.sabyan_ibadallah)
+            Model(1, "Ibadallah", "Nissa", R.drawable.nissa1, R.raw.sabyan_ibadallah)
         )
 
         dataMp3.add(
-            Model(2, "Rahman ya Rahman", "Nissa", R.drawable.nissa2, R.raw.sabyanalma_al_musthofa)
+            Model(2, "Al Musthofa", "Nissa X Alma", R.drawable.nissa2, R.raw.sabyanalma_al_musthofa)
         )
 
         dataMp3.add(
-            Model(3, "Ya Asyiqol Musthofa", "Nissa", R.drawable.nissa3, R.raw.sabyan_ibadallah)
+            Model(3, "Huwannur", "Nissa", R.drawable.nissa3, R.raw.huwannur)
         )
 
         dataMp3.add(
-            Model(4, "Ya Maulana", "Nissa", R.drawable.nissa4, R.raw.sabyanalma_al_musthofa)
+            Model(4, "Ya Muhaimin", "Nissa", R.drawable.nissa4, R.raw.ya_muhaimin)
         )
 
         dataMp3.add(
-            Model(5, "Deen Assalam", "Nissa", R.drawable.nissa5, R.raw.sabyan_ibadallah)
+            Model(5, "Ala Bali", "Nissa", R.drawable.nissa5, R.raw.ala_bali)
         )
 
         dataMp3.add(
-            Model(6, "Ya Jamalu", "Nissa", R.drawable.nissa6, R.raw.sabyanalma_al_musthofa)
+            Model(6, "Albi Nadak", "Nissa", R.drawable.nissa6, R.raw.albi_nadak)
         )
 
         dataMp3.add(
-            Model(7, "Law Kana Bainanal Habib Ya Salam Alhamdu", "Nissa", R.drawable.nissa7, R.raw.sabyan_ibadallah)
+            Model(7, "Teman Sejati", "Nissa X Tasya Rosmala", R.drawable.nissa7, R.raw.teman_sejati)
         )
 
         dataMp3.add(
-            Model(8, "Ahmad Ya Habibi", "Nissa", R.drawable.nissa8, R.raw.sabyanalma_al_musthofa)
+            Model(8, "Fatimah Az Zahra", "Nissa", R.drawable.nissa8, R.raw.fatimah_az_zahra)
         )
 
         dataMp3.add(
-            Model(9, "Ya Taiba", "Nissa", R.drawable.nissa9, R.raw.sabyan_ibadallah)
+            Model(9, "Aisyah", "Nissa", R.drawable.nissa9, R.raw.aisyah)
         )
 
         dataMp3.add(
-            Model(10, "Qomarun", "Nissa", R.drawable.nissa10, R.raw.sabyanalma_al_musthofa)
+            Model(10, "Addinu Lana", "Nissa", R.drawable.nissa10, R.raw.addinu_lana)
         )
 
         return dataMp3
